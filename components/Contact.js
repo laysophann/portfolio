582ac3bea7734fe8451c11684587148a2edb0c06
laysophann <lay.sophann19@kit.edu.kh>
2023-08-30 16:05:43 +0700

    add vertical timeline of experience

--- components/Contact.js
+++ components/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
   return (
     <section id="contact" className="py-20">
       <div className="container mx-auto text-center font-mono">
-        <h2 className="text-3xl font-bold text-white mb-6">Contact Me</h2>
+        <h2 className="text-3xl font-bold text-white mb-6">Connect Me ()</h2>
         <p className="text-lg text-gray-400 mb-10 mx-3">
           Want to get in touch? Feel free to reach out to me via email or connect with me
           on social media. I'm always excited to collaborate on new projects and
