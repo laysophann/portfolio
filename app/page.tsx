9a5438f59e7a76a3d3630b5c44f4a2f933e68b83
laysophann <lay.sophann19@kit.edu.kh>
2023-08-31 22:56:05 +0700

    add smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
+import React from "react";
+import { motion } from "framer-motion";
 import NightSkyBackground from "../components/NightSkyBackground";
 import Navigation from "../components/Navigation";
 import About from "../components/About";
 import Work from "../components/Work";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
+import { BiSolidArrowToTop } from "react-icons/bi";
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
 export default function Home() {
   return (
@@ -19,6 +26,15 @@ export default function Home() {
       <Work />
       <Skills />
       <Contact />
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        className="text-gray-400 text-2xl md:text-3xl lg:text-4xl float-right mr-10 mb-10 animate-bounce cursor-pointer scroll-smooth"
+        onClick={scrollToTop}
+      >
+        <BiSolidArrowToTop />
+      </motion.div>
     </div>
   );
 }
--- components/Contact.js
+++ components/Contact.js
@@ -1,12 +1,36 @@
 import React from "react";
 
 const skillsData = [
-  { title: "Email", icon: "email.png", link: "https://mail.google.com/mail/u/1/#inbox" },
-  { title: "Linkedin", icon: "linkedin.png", link: "https://www.linkedin.com/in/lay-sophann-7750b8205/" },
-  { title: "Twitter", icon: "twitter.png", link: "https://twitter.com/sophannlay" },
-  { title: "Instagram", icon: "instagram.png", link: "https://www.instagram.com/laysophann/" },
-  { title: "Facebook", icon: "facebook.png", link: "https://www.facebook.com/sophann.lay.967" },
-  { title: "Telegram", icon: "telegram.png", link: "https://web.telegram.org/k/" },
+  {
+    title: "Email",
+    icon: "email.png",
+    link: "https://mail.google.com/mail/u/1/#inbox",
+  },
+  {
+    title: "Linkedin",
+    icon: "linkedin.png",
+    link: "https://www.linkedin.com/in/lay-sophann-7750b8205/",
+  },
+  {
+    title: "Twitter",
+    icon: "twitter.png",
+    link: "https://twitter.com/sophannlay",
+  },
+  {
+    title: "Instagram",
+    icon: "instagram.png",
+    link: "https://www.instagram.com/laysophann/",
+  },
+  {
+    title: "Facebook",
+    icon: "facebook.png",
+    link: "https://www.facebook.com/sophann.lay.967",
+  },
+  {
+    title: "Telegram",
+    icon: "telegram.png",
+    link: "https://web.telegram.org/k/",
+  },
 ];
 
 function Contact() {
@@ -15,12 +39,12 @@ function Contact() {
       <div className="container mx-auto text-center font-mono">
         <h2 className="text-3xl font-bold text-white mb-6">Connect Me ()</h2>
         <p className="text-lg text-gray-400 mb-10 mx-3">
-          Want to get in touch? Feel free to reach out to me via email or connect with me
-          on social media. I am always excited to collaborate on new projects and
-          opportunities!
+          Want to get in touch? Feel free to reach out to me via email or
+          connect with me on social media. I am always excited to collaborate on
+          new projects and opportunities!
         </p>
         <div className="flex flex-wrap justify-center items-center gap-x-4">
-          {skillsData.map(skill => (
+          {skillsData.map((skill) => (
             <a
               key={skill.title}
               href={skill.link}
