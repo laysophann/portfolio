502be1faed0504e23436ed25d0ee330f90bf837a
laysophann <lay.sophann19@kit.edu.kh>
2023-08-31 12:09:59 +0700

    mobile version add hamburger icon

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,40 +1,92 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../app/globals.css";
+import useMediaQuery from "./useMedieQuery";
 
 function Navigation() {
-  const [isNavFixed, setIsNavFixed] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  console.log(isNavOpen);
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+  // console.log(isDesktop)
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsNavFixed(window.scrollY > 50); // Adjust the scroll threshold as needed
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
-    <nav
-      className={`flex justify-between items-center p-4 ${
-        isNavFixed ? "fixed-nav" : ""
-      }`}
-    >
-      <h1 className="font-bold text-5xl cursor-pointer bg-slate-800 rounded-lg border-x">.SP.</h1>
-      <ul className="flex space-x-10 text-lg">
-        <li className="hover:bg-slate-800 rounded-lg p-3">
-          <a href="#about">About</a>
-        </li>
-        <li className="hover:bg-slate-800 rounded-lg p-3">
-          <a href="#work">Work</a>
-        </li>
-        <li className="hover:bg-slate-800 rounded-lg p-3">
-          <a href="#skills">Skills</a>
-        </li>
-        <li className="hover:bg-slate-800 rounded-lg p-3">
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+    <nav className={`p-4 ${isNavOpen ? "fixed-nav" : ""}`}>
+      <div className="flex justify-between items-center">
+        <h1 className="SP font-bold text-5xl cursor-pointer">.SP.</h1>
+
+        {isDesktop ? (
+          <ul
+            className={`text-lg text-white font-semibold space-x-10 flex items-center`}
+          >
+            <li className="hover:text-gray-400 rounded-lg p-3">
+              <a href="#about" onClick={toggleNav}>
+                About
+              </a>
+            </li>
+            <li className="hover:text-gray-400 rounded-lg p-3">
+              <a href="#work" onClick={toggleNav}>
+                Work
+              </a>
+            </li>
+            <li className="hover:text-gray-400 rounded-lg p-3">
+              <a href="#skills" onClick={toggleNav}>
+                Skills
+              </a>
+            </li>
+            <li className="hover:text-gray-400 rounded-lg p-3">
+              <a href="#contact" onClick={toggleNav}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        ) : (
+          <div>
+            {isNavOpen ? (
+              <FaTimes
+                onClick={toggleNav}
+                className="text-gray-400 cursor-pointer"
+              />
+            ) : (
+              <FaBars
+                onClick={toggleNav}
+                className="text-gray-400 cursor-pointer"
+              />
+            )}
+          </div>
+        )}
+      </div>
+      {isNavOpen ? (
+        <ul
+          className={`mt-4 flex flex-col justify-center items-center
+        text-lg text-white font-semibold`}
+        >
+          <li className="hover:text-gray-400 rounded-lg p-3">
+            <a href="#about" onClick={toggleNav}>
+              About
+            </a>
+          </li>
+          <li className="hover:text-gray-400 rounded-lg p-3">
+            <a href="#work" onClick={toggleNav}>
+              Work
+            </a>
+          </li>
+          <li className="hover:text-gray-400 rounded-lg p-3">
+            <a href="#skills" onClick={toggleNav}>
+              Skills
+            </a>
+          </li>
+          <li className="hover:text-gray-400 rounded-lg p-3">
+            <a href="#contact" onClick={toggleNav}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      ) : (
+        ""
+      )}
     </nav>
   );
 }
