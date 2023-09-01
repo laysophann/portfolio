e2a3a9d13f2ca5c72a6c5efd9a1c624ad1d9e14d
laysophann <lay.sophann19@kit.edu.kh>
2023-09-02 01:21:50 +0700

    fixed icon not display in production mode

--- utils/ExperienceData.js
+++ utils/ExperienceData.js
@@ -15,7 +15,7 @@ const experiences = [
       date: "2021-02 - 2022-04",
       title: "Data Annotator",
       company_name: "Client Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Tool : Post Annotation and V7 ",
@@ -26,7 +26,7 @@ const experiences = [
       date: "2021-0304 - 2022-02-04",
       title: "Frontend Developer",
       company_name: "OGP Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Develop responsive web applications using Refine.Js",
@@ -37,7 +37,7 @@ const experiences = [
       date: "2022-01-24 - 2022-03-07",
       title: "Flutter Developer",
       company_name: "Training Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Programming used : Flutter",
@@ -48,7 +48,7 @@ const experiences = [
       date: "2022-08-29 - 2022-10-31",
       title: "Frontend Developer",
       company_name: "Client Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Programming used : Three.JS",
@@ -59,7 +59,7 @@ const experiences = [
       date: "2021-12-15 - 2022-02-15",
       title: "IC(Incubation Center) support",
       company_name: "KIT",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Responsible for uploading apps to AppStore and PlayStore",
@@ -70,7 +70,7 @@ const experiences = [
       date: "2022-04-19 - 2022-06-18",
       title: "Mobile App developer",
       company_name: "Training Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Tool : BuildBox",
@@ -81,7 +81,7 @@ const experiences = [
       date: "2022-09-12 - 2023-04-15",
       title: "Shopify Frontend Web developer",
       company_name: "Client Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Programming languages : Polaris, Liquid , React.Js and Typescript",
@@ -92,7 +92,7 @@ const experiences = [
       date: "2022-11-14 - 2023-01-02",
       title: "Desktop App developer",
       company_name: "Training Project",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Programming language : Electron.Js",
@@ -103,7 +103,7 @@ const experiences = [
       date: "2023-05-19 - 2023-07-21",
       title: "Frontend developer",
       company_name: "Bootcamp fullstack Web development",
-      icon: "/icons/kitlogo.png",
+      icon: "/icons/kit.png",
       iconBg: "#1d1836",
       points: [
         "Programming languages : React.Js and Typescript",
