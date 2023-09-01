8f0443780572defc2611818467bf3f6050e31029
laysophann <lay.sophann19@kit.edu.kh>
2023-09-01 14:58:39 +0700

    add Projects data

--- components/Skills.js
+++ components/Skills.js
@@ -1,29 +1,36 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "../app/globals.css";
 import skills from "../utils/SkillsData";
 
 function Skills() {
   return (
-    <section id="skills" className="py-20 font-mono">
-    <div className="container mx-auto text-center">
-      <h2 className="text-3xl font-bold text-white mb-6">Skills</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {skills.map(skill => (
-          <div
-            key={skill.title}
-            className="p-4 rounded-lg"
-          >
-            <img
-              src={`/images/${skill.icon}`}
-              alt={`${skill.title} Icon`}
-              className="mx-auto h-12 mb-4 animate-spin-hover"
-            />
-            <h3 className="text-lg font-semibold text-white">{skill.title}</h3>
-          </div>
-        ))}
+    <motion.div
+      id="skills"
+      className="py-20 font-mono"
+      initial={{ opacity: 0 }} // Initial animation state
+      animate={{ opacity: 1 }} // Animation when component is present
+      exit={{ opacity: 0 }} // Animation when component is removed
+      transition={{ duration: 0.1 }} // Animation duration
+    >
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-bold text-white mb-6">Skills</h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {skills.map((skill) => (
+            <motion.div key={skill.title} className="p-4 rounded-lg">
+              <img
+                src={`/images/${skill.icon}`}
+                alt={`${skill.title} Icon`}
+                className="mx-auto h-12 mb-4 animate-spin-hover"
+              />
+              <h3 className="text-lg font-semibold text-white">
+                {skill.title}
+              </h3>
+            </motion.div>
+          ))}
+        </div>
       </div>
-    </div>
-  </section>
+    </motion.div>
   );
 }
 
