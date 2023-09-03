1b3c49e33191eb7ceef7f2f3c3bab6dc022b7929
laysophann <lay.sophann19@kit.edu.kh>
2023-09-03 10:34:14 +0700

    add preloader screen

---
+++ preloader/Preloader.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "../app/globals.css";
+
+function Preloader() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading by setting a timeout
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 300); // Change the duration as needed
+  }, []);
+
+  return (
+    <motion.div
+      className={`preloader ${isLoading ? "loading" : ""}`}
+      initial={{ opacity: 1 }}
+      animate={{ opacity: isLoading ? 1 : 0, transition: { duration: 0.5 } }}
+    >
+      <div className="loader"></div>
+    </motion.div>
+  );
+}
+
+export default Preloader;
