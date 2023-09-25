6bad374477940bee5fac05c587f0e906f61be19a
laysophann <lay.sophann19@kit.edu.kh>
2023-09-25 23:36:34 +0700

    update readMe file

--- components/Project.jsx
+++ components/Project.jsx
@@ -12,7 +12,6 @@ function Project() {
         className="container mx-auto text-center"
         style={{ maxWidth: "1300px" }}
       >
-        {/* Set maxWidth to "1300px" if you prefer that */}
         <h2 className="text-3xl font-bold text-white mb-6">Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project, index) => (
